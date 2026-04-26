a9ff9e1d17c0c81cb26c67effff874772b26b8fb
Umar-azeem <umara4436@gmail.com>
2026-04-26 06:41:51 +0500

    pkg

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -47,7 +47,7 @@ export default function Main() {
               <div className="relative inline-flex mt-6 group">
                 <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-full blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
                 <a
-                  href="https://drive.google.com/file/d/1oqGi7DXULYdTi0rvi5omf_M8ez5gxLz1/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1f05Iz865yDfaKuTC92b1QR2kT97gRwLk/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="relative text-gray-500 hover:text-cyan-500 inline-flex items-center justify-center px-6 py-3 sm:px-8 sm:py-4 text-base sm:text-lg font-semibold transition-all duration-200 bg-gray-950 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
